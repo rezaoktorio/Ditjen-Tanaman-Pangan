8c75b4aefa060c0884b0b63e111ebd387fa167d0
rezaoktorio <rezasynt@gmail.com>
2018-04-11 16:50:53 +0700

    leftbar

---
+++ E-Surat/repo.php
@@ -0,0 +1,125 @@
+<ul class="list-unstyled">
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-buttons.html">Buttons</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-loading-buttons.html">Loading Buttons</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-panels.html">Panels</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-portlets.html">Portlets</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-checkbox-radio.html">Checkboxs-Radios</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-tabs.html">Tabs</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-modals.html">Modals</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-progressbars.html">Progress Bars</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-notification.html">Notification</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-images.html">Images</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-carousel.html">Carousel</a>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-video.html">Video</a>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-bootstrap.html">Bootstrap UI</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/ui-typography.html">Typography</a></li>
+
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-grid.html">Grid</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-widgets.html">Widgets</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-nestable-list.html">Nesteble</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-range-sliders.html">Range sliders</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-masonry.html">Masonry</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-animation.html">Animation</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-sweet-alert.html">Sweet Alerts</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-treeview.html">Treeview</a></li>
+  <li><a href="http://tp.webplussolution.com/E-Surat/components-tour.html">Tour</a></li>
+
+  <li><a href="http://tp.webplussolution.com/E-Surat/icons-glyphicons.html">Glyphicons</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-materialdesign.html">Material Design</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-ionicons.html">Ion Icons</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-fontawesome.html">Font awesome</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-themifyicon.html">Themify Icons</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-simple-line.html">Simple line Icons</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-weather.html">Weather Icons</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/icons-typicons.html">Typicons</a></li>
+
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-elements.html">General Elements</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-advanced.html">Advanced Form</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-validation.html">Form Validation</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-pickers.html">Form Pickers</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-wizard.html">Form Wizard</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-mask.html">Form Masks</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-summernote.html">Summernote</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-wysiwig.html">Wysiwig Editors</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-code-editor.html">Code Editor</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-uploads.html">Multiple File Upload</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-xeditable.html">X-editable</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/form-image-crop.html">Image Crop</a></li>
+
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-basic.html">Basic Tables</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-datatable.html">Data Table</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-editable.html">Editable Table</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-responsive.html">Responsive Table</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-foo-tables.html">FooTable</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-bootstrap.html">Bootstrap Tables</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-tablesaw.html">Tablesaw Tables</a></li>
+    <li><a href="http://tp.webplussolution.com/E-Surat/tables-jsgrid.html">JsGrid Tables</a></li>
+
+    <li><a href="http://tp.webplussolution.com/E-Surat/chart-flot.html">Flot Chart</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-morris.html">Morris Chart</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-chartjs.html">Chartjs</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-peity.html">Peity Charts</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-chartist.html">Chartist Charts</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-c3.html">C3 Charts</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-nvd3.html"> Nvd3 Charts</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-sparkline.html">Sparkline charts</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-radial.html">Radial charts</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-other.html">Other Chart</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/chart-ricksaw.html">Ricksaw Chart</a></li>
+
+      <li><a href="http://tp.webplussolution.com/E-Surat/map-google.html"> Google Map</a></li>
+      <li><a href="http://tp.webplussolution.com/E-Surat/map-vector.html"> Vector Map</a></li>
+
+      <li><a href="http://tp.webplussolution.com/E-Surat/page-starter.html">Starter Page</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-login.html">Login</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-login-v2.html">Login v2</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-register.html">Register</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-register-v2.html">Register v2</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-signup-signin.html">Signin - Signup</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-recoverpw.html">Recover Password</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-lock-screen.html">Lock Screen</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-400.html">Error 400</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-403.html">Error 403</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-404.html">Error 404</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-404_alt.html">Error 404-alt</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-500.html">Error 500</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/page-503.html">Error 503</a></li>
+
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-profile.html">Profile</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-timeline.html">Timeline</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-sitemap.html">Site map</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-invoice.html">Invoice</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-email-template.html">Email template</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-maintenance.html">Maintenance</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-coming-soon.html">Coming-soon</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-faq.html">FAQ</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-search-result.html">Search result</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-gallery.html">Gallery</a></li>
+        <li><a href="http://tp.webplussolution.com/E-Surat/extra-pricing.html">Pricing</a></li>
+
+          <li><a href="http://tp.webplussolution.com/E-Surat/apps-calendar.html"> Calendar</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/apps-contact.html"> Contact</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/apps-taskboard.html"> Taskboard</a></li>
+
+          <li><a href="http://tp.webplussolution.com/E-Surat/email-inbox.html"> Inbox</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/email-read.html"> Read Mail</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/email-compose.html"> Compose Mail</a></li>
+
+          <li><a href="http://tp.webplussolution.com/E-Surat/layout-leftbar_2.html"> Leftbar with User</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/layout-menu-collapsed.html"> Menu Collapsed</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/layout-menu-small.html"> Small Menu</a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/layout-header_2.html"> Header style</a></li>
+
+          <li><a href="http://tp.webplussolution.com/E-Surat/crm-dashboard.html"> Dashboard </a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/crm-contact.html"> Contacts </a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/crm-opportunities.html"> Opportunities </a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/crm-leads.html"> Leads </a></li>
+          <li><a href="http://tp.webplussolution.com/E-Surat/crm-customers.html"> Customers </a></li>
+
+            <li><a href="http://tp.webplussolution.com/E-Surat/ecommerce-dashboard.html"> Dashboard</a></li>
+            <li><a href="http://tp.webplussolution.com/E-Surat/ecommerce-products.html"> Products</a></li>
+            <li><a href="http://tp.webplussolution.com/E-Surat/ecommerce-product-detail.html"> Product Detail</a></li>
+            <li><a href="http://tp.webplussolution.com/E-Surat/ecommerce-product-edit.html"> Product Edit</a></li>
+            <li><a href="http://tp.webplussolution.com/E-Surat/ecommerce-orders.html"> Orders</a></li>
+            <li><a href="http://tp.webplussolution.com/E-Surat/ecommerce-sellers.html"> Sellers</a></li>
+</ul>
