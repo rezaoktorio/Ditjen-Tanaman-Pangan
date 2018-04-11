6b1edecaa7e5e9ef6bb0c1203c4328c0fae895b5
rezaoktorio <rezasynt@gmail.com>
2018-04-11 07:57:49 +0700

    adding fill.php tiap model->folder

---
+++ E-Surat/model/carisurat/fill.php
@@ -0,0 +1,35 @@
+<div class="content-page">
+    <!-- Start content -->
+    <div class="content">
+        <div class="container">
+
+            <!-- Page-Title -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="btn-group pull-right m-t-15">
+                        <button type="button" class="btn btn-default dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="false">Settings <span class="m-l-5"><i class="fa fa-cog"></i></span></button>
+                        <ul class="dropdown-menu drop-menu-right" role="menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </div>
+
+                    <h4 class="page-title">Dashboard</h4>
+                    <p class="text-muted page-title-alt">Welcome to Ubold admin panel !</p>
+                </div>
+            </div>
+
+          <!-- row content -->
+
+          <!-- row content -->
+
+        </div> <!-- container -->
+
+    </div> <!-- content -->
+
+    <?php require_once('../../controller/footer.php'); ?>
+
+</div>
